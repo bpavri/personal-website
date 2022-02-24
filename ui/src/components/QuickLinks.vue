b046541b98945791ceeca820f7375292daa8c639
BPavri <burjiss85@gmail.com>
2022-02-24 00:49:53 -0800

    FInished home page and resume page

--- ui/src/components/QuickLinks.vue
+++ ui/src/components/QuickLinks.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="quick-links">
-    <h1>{{ msg }}</h1>
-    <p>
+    <h1 class="mb-4">{{ msg }}</h1>
+    <h5 class="mb-5">
       Burjiss is a Senior Software Engineer out of Seattle, Washington<br>
-    </p>
-    <!-- <h3>Quick Links</h3>
-    <ul>
-      <li><a href="/resume" target="_blank" rel="noopener">Resume</a></li>
-      <li><a href="/projects" target="_blank" rel="noopener">Projects</a></li>
-    </ul> -->
+    </h5>
+    <!-- For some spacing -->
+    <div class="mt-5 mb-5" style="color: white;"><p>.</p></div>
 
-    <!-- <img src="../assets/seattle.jpg" style="position: relative;
-    left: 0;
-    width: 100%;
-    bottom: -8em;
-    height: auto" loading="lazy" /> -->
+    <div class="container mt-5 mb-3">
+      <div class="row">
+        <div class="col"><img class="img-fluid" width="70em" src="../assets/python.svg" /></div>
+        <div class="col"><img class="img-fluid" width="85em" src="../assets/flask.png" /></div>
+        <div class="col"><img class="img-fluid" width="90em" src="../assets/php.png" /></div>
+        <div class="col"><img class="img-fluid" width="90em" src="../assets/laravel.png" /></div>
+        <div class="col"><img class="img-fluid" width="90em" src="../assets/drupal.png" /></div>  
+      </div>
+
+  <div class="row mt-4">
+        <div class="col"><img class="img-fluid" width="80em" src="../assets/docker.png" /></div>
+        <div class="col"><img class="img-fluid" width="85em" src="../assets/kubernetes.png" /></div>
+        <div class="col"><img class="img-fluid" width="80em" src="../assets/vuejs.png" /></div>
+        <div class="col"><img class="img-fluid" width="100em" src="../assets/salesforce.png" /></div>
+      </div>
+
+    </div>
   </div>
 </template>
 
--- ui/src/pages/Resume/Resume.vue
+++ ui/src/pages/Resume/Resume.vue
@@ -1,29 +1,289 @@
 <template>
   <div id="root" class="container px-0 min-vh-100 shadow-lg">
-    <NavBar class="mb-5"/>
-Resume
+    <NavBar />
+
+    <div class="container-fluid pb-5">
+      <div class="row header">
+        <div class="col-md-auto gx-0">
+          <img src="../../assets/portrait.png" />
+        </div>
+        <div class="col-6 middle">
+          <div class="row px-4 py-4">
+            <div class="col-auto">
+              <h1 class="mb-3">BURJISS PAVRI</h1>
+              <span class="fs-4">Senior Software Engineer</span>
+
+              <ul class="list-group list-group-flush mt-5">
+                <li class="list-group-item">
+                  <i
+                  class="bi-mailbox"
+                  style="font-size: 1.25rem; color: white"
+                ></i>
+                  burjiss85@gmail.com</li> 
+                <li class="list-group-item">
+                  <i
+                  class="bi-phone"
+                  style="font-size: 1.25rem; color: white"
+                ></i>
+                  213-400-2438</li>
+              </ul>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col right">
+          <ul class="list-group list-group-flush mt-5">
+                <li class="list-group-item">
+                  <i
+                  class="bi-linkedin"
+                  style="font-size: 1.25rem; color: white"
+                ></i>
+                  www.linkedin.com/in/bpav1/</li> 
+                <li class="list-group-item">
+                  <i
+                  class="bi-git"
+                  style="font-size: 1.25rem; color: white"
+                ></i>
+                  github.com/bpavri</li>
+                  <li class="list-group-item">
+                  <i
+                  class="bi-display"
+                  style="font-size: 1.25rem; color: white"
+                ></i>
+                  www.burjiss.com/knockknock</li>
+              </ul>
+        </div>
+      </div>
+
+      <div class="row mt-5 content">
+        <div class="col-7 mx-4 work mb-5">
+          <h4 class="border-bottom border-dark pb-2 mb-4">
+            WORK EXPERIENCE
+          </h4>
+          
+          
+          <h5>Software Engineer</h5>
+          <h6 class="text-muted">Honorsociety.org, 2013 - Present</h6>
+          <ul class="my-4">
+            <li>Envisioned and developed feature sets like benefits redemption, scholarship directory, micro blogging platform and news feed.</li>
+            <li>Developed innovative solutions to resource limitations with Laravel Queues and delayed ARB (recurring charges) revoking.</li>
+            <li>Served as technical lead and developer for integrations with various ESPs (EDirect/Campaigner/Mailchimp), CRM (Salesforce) and a NoSQL (Elastic Stack) datastore.</li>
+            <li>Developed and setup a tiered subscription payment system with Authorize.net as the payment gateway.</li>
+            <li>Managing product development with ticketing system using user stories, sprints, milestones, prototyping and technical wiki.</li>
+            <li>Managed team of onshore and offshore technical assets and teams for several of the above feature developments using Agile methodologies.</li>
+          </ul>
+          
+          <span class="badge bg-secondary">Laravel</span>
+          <span class="badge bg-secondary">Vuejs</span>
+          <span class="badge bg-secondary">Drupal</span>
+
+
+          <h5 class="mt-5">Director of Technology</h5>
+          <h6 class="text-muted">Mortgage Panda, 2016 - 2021</h6>
+          <ul class="my-4">
+            <li>Developed the worlds first and only Mortgage Loan Origination System built natively on Salesforce.</li>
+            <li>Features developed include mortgage ratio and fees calculations, architecting salesforce objects for a full mortgage application (1003), integrations with Credit Vendors, Point of Sale systems and Document auditing services.</li>
+            <li>Setup common mortgage reporting with KPIs with the ability to use native Salesforce reporting to extend and build on our package provided mortgage objects.</li>
+            <li>Performed full app security review for app exchange listing.</li>
+          </ul>
+
+          <span class="badge bg-secondary">Salesforce Sales Cloud</span>
+          <span class="badge bg-secondary">Apex</span>
+          <span class="badge bg-secondary">Lightning</span>
+
+
+          <h5 class="mt-5">Software Developer</h5>
+          <h6 class="text-muted">Guidance Solutions, 2010 - 2013</h6>
+          <ul class="my-4">
+            <li>Developed e-commerce stores using several open source technologies.</li>
+            <li>Worked on various supporting social applications deployed on the Amazon EC2 platform.</li>
+          </ul>
+
+          <span class="badge bg-secondary">Drupal</span>
+          <span class="badge bg-secondary">Magento</span>
+          <span class="badge bg-secondary">AWS</span>
+
+        </div>
+
+        <div class="col-4 mx-4">
+<div class="container">
+  <div class="row">
+    <div class="col">
+          <h4 class="border-bottom border-dark pb-2">
+            SKILLS
+          </h4>
+
+       <div class="progress mb-3 mt-4">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    PHP
+  </div>
+</div>
+      
+       <div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    DRUPAL
+  </div>
+</div>
+
+<div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 80%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    LARAVEL
+  </div>
+</div>
+
+<div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 80%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    PYTHON
+  </div>
+</div>
+
+<div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 50%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    FLASK
+  </div>
+</div>
+
+<div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 80%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    VUE.JS
+  </div>
+</div>
+
+<div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    SALESFORCE, APEX
+  </div>
+</div>
+
+<div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 80%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    DOCKER, GIT
+  </div>
+</div>
+
+<div class="progress mb-3">
+  <div class="fw-bold progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 25%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+    KUBERNETES
+  </div>
+</div>
+</div>
+</div> 
+<div class="row">
+<div class="col">
+  <h4 class="border-bottom border-dark pb-2 mt-5 mb-4">
+            EDUCATION
+          </h4>
+
+<h5>MSc in Computer Science</h5>
+          <h6 class="text-muted mb-3">University of Southern California, 2009</h6>
+
+          <h5>BEng in Computer Engineering</h5>
+          <h6 class="text-muted">University of Mumbai, 2007</h6>
+
+</div>
+</div>
+
+
+<div class="row">
+<div class="col">
+  <h4 class="border-bottom border-dark pb-2 mt-5 mb-4">
+            JUNIOR EXPERIENCE
+          </h4>
+
+<h5 class="">Digital Playspace, 2009 - 2010</h5>
+          
+          <ul class="my-4">
+            <li>Migrated from Drupal 5 to Drupal 6.</li>
+            <li>Optimized Drupal & Flash resulting in documented decrease in play area times.</li>
+          </ul>
+
+          <span class="badge bg-secondary">Drupal</span>
+
+          <h5 class="mt-4">Campusbuddy, 2009</h5>
+          
+          <ul class="my-4">
+            <li>Forum development using VBulletin.</li>
+            <li>General web development in CakePhp</li>
+          </ul>
+
+          <span class="badge bg-secondary">Php</span>
+
+          <h5 class="mt-4">Los Angeles Times, Summer 2008</h5>
+          
+          <ul class="my-4">
+            <li>Geo tagged blogs by parsing content.</li>
+            <li>Developed a Facebook app which incorporated the geo-spatial blog info that could be shared with friends.</li>
+          </ul>
+
+          <span class="badge bg-secondary">Ruby on Rails</span>
+
+</div>
+</div>
+
+
+
+        </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-import NavBar from '../../components/NavBar.vue'
+import NavBar from "../../components/NavBar.vue";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default {
-  name: 'Resume',
+  name: "Resume",
   components: {
-    NavBar
-  }
-}
+    NavBar,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.header {
+  color: white;
+  background-color: rgb(69, 78, 93);
+}
+
+.header img {
+  max-width: 18rem;
+  height: auto;
+}
+
+.header ul li {
+  background: none;
+  color: white;
+  border-color: white;
+}
+
+.header .middle {
+  text-align: left;
+}
+
+.header .right ul li {
+  text-align: left;
+  border-color: rgb(69, 78, 93);
+}
+
+.content{
+  text-align: left;
+}
+
+.badge {
+  font-size: 1rem;
+  margin: 0 0.5rem 0 0.5rem;
+  padding: 0.25rem 0.5rem 0.25rem 0.5rem;
+}
+
 #root {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;;
+  color: #2c3e50;
   /* margin-top: 60px; */
   background-color: white;
 }
